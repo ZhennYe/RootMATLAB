7ee41c8dd5f8540627a4f26124577713ef1f69e9
Will Savoie <wsavoie@gatech.edu>
2017-08-31 11:56:29 -0400

    extra varargin for parseFileNames which allows stripping of file extension

--- parseFileNames.m
+++ parseFileNames.m
@@ -1,5 +1,5 @@
-function [pars,vals]=parseFileNames(fname)
-% function []=parseFileNames(fname)
+function [pars,vals]=parseFileNames(fname,varargin)
+% if second argument exists, and is equal to 1, strips file extension
 % spk_#1_smrt_#2_gait_#3_rob_#4_v_#5
 % 
 % #1= speaker position in degrees
@@ -8,7 +8,10 @@ function [pars,vals]=parseFileNames(fname)
 % #4= number of robots
 % #5= iteration of that run
 
-str = fname;
+if(nargin>1&&varargin{1}==1)
+[~,fname,~]=fileparts(fname);
+end
+
 %get strings
 expression = '[a-z A-Z]*';
 pars = regexp(fname,expression,'match');
