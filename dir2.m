fc5427c9ad20c637f30b889b74035ae5976c87a8
PHYS33229\root <wsavoie@gatech.edu>
2017-12-04 10:51:53 -0500

    added some comments to dir2 and ability to take only files/folders

--- dir2.m
+++ dir2.m
@@ -1,4 +1,7 @@
 function out=dir2(folder,varargin)
+%if no varargin take all entries
+%if varargin{1} == 'folders' only take folders
+%if varargin{1} ~= 'folders' take only files
 out=dir(folder);
 out=out(~ismember({out.name},{'.','..'}));
 
@@ -6,6 +9,8 @@ if length(varargin)>0
     for i=1:length(varargin)
         if(varargin{i}=='folders') %only take folders
             out=out(find([out(:).isdir]));
+        else
+            out=out(find(~[out(:).isdir]));
         end
     end
 end
